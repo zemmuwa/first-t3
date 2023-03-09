398dc5f1287988b64c80a969d86a5e43cb464b20
Zemmy Muamal Humaidi <neo.spacians@gmail.com>
2023-03-09 10:16:37 +0700

    add: convert webp

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -2,13 +2,15 @@ import nextConnect from "next-connect";
 import multer from "multer";
 import { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
-
+import sharp from "sharp";
+import * as fs from "fs";
 const upload = multer({
   storage: multer.diskStorage({
     destination: "./public/uploads",
     filename: (req, file, cb) => cb(null, file.originalname),
   }),
 });
+const name = String(Date.now())
 
 const apiRoute = nextConnect({
   onError(error, req: NextApiRequest, res: NextApiResponse) {
@@ -21,21 +23,40 @@ const apiRoute = nextConnect({
   },
 });
 
-apiRoute.use(upload.array("theFiles"));
+apiRoute.use(
+  upload.array("theFiles"),
+  async (req: NextApiRequest, res: NextApiResponse, next) => {
+    const {
+      filename: image,
+      path,
+      destination,
+    } = (req as NextApiRequest & { files: any[] }).files?.[0];
+    console.log([path, image, destination]);
+    await sharp(path)
+      .webp()
+      .toFile("public\\uploads\\" + `${name}.webp`);
+    fs.unlinkSync(path);
+    next();
+  }
+);
 
-apiRoute.post( async(req, res) => {
-    const file = (req as NextApiRequest & { files: any[] }).files?.[0]
-  console.log();
-  const prisma = new PrismaClient()
+apiRoute.post(async (req, res) => {
+  const file = (req as NextApiRequest & { files: any[] }).files?.[0];
+  const prisma = new PrismaClient();
+  const {size,} = fs.statSync("public\\uploads\\" + `${name}.webp`);
   const posts = await prisma.files.create({
-    data:{
-        filename:file.filename,
-        originalname:file.originalname,
-        path:file.path,
-        mimetype:file.mimetype,
-        size:file.size,
-    }
-  })
+    data: {
+      // filename: file.filename,
+      filename: `${name}.webp`,
+      originalname: file.originalname,
+      // path: file.path,
+      path: "public\\uploads\\" + `${name}.webp`,
+      // mimetype: file.mimetype,
+      mimetype: 'image/webp',
+      // size: file.size,
+      size: size,
+    },
+  });
   res.status(200).json({ data: "success" });
 });
 
